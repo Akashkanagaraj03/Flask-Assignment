797ec18f342be9db2b7686f1873e9149d0316b0a
akash <akashkanagaraj03@gmail.com>
2025-02-20 11:10:34 +0530

    Added database population from .json

--- src/models.py
+++ src/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 from sqlalchemy import create_engine
+
+engine = create_engine("sqlite:///database.db")
 
 
 class Base(DeclarativeBase):
@@ -20,5 +22,42 @@ class User(Base):
     age: Mapped[int] = mapped_column()
 
 
-engine = create_engine("sqlite:///database.db", echo=True)
-Base.metadata.create_all(engine)
+def create_user(
+    id, first_name, last_name, company_name, city, state, zip, email, web, age
+):
+    session = Session(engine)
+    with session.begin():
+        try:
+            session.add(
+                User(
+                    id=id,
+                    first_name=first_name,
+                    last_name=last_name,
+                    company_name=company_name,
+                    city=city,
+                    state=state,
+                    zip=zip,
+                    email=email,
+                    web=web,
+                    age=age,
+                )
+            )
+        except Exception as e:
+            print(f"Error:{e}")
+            session.rollback()
+
+        else:
+            session.commit()
+            print(f"Data with user id:{id} created.")
+
+        finally:
+            session.close()
+    return
+
+
+def main():
+    Base.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/populate_db.py
@@ -0,0 +1,25 @@
+import json
+from models import create_user
+
+
+def main():
+    with open("users.json") as file:
+        users = json.load(file)
+
+        for user in users:
+            create_user(
+                user["id"],
+                user["first_name"],
+                user["last_name"],
+                user["company_name"],
+                user["city"],
+                user["state"],
+                user["zip"],
+                user["email"],
+                user["web"],
+                user["age"],
+            )
+
+
+if __name__ == "__main__":
+    main()
